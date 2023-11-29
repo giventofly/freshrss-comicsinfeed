f7d259520fbfc965226f5546b6620770ea92dc63
Craig Astill <jackson15j@hotmail.com>
2023-11-29 14:08:57 +0000

    chore: Added Penny-Arcade parser to extension entrypoint code.

--- comics/loader.php
+++ comics/loader.php
@@ -6,5 +6,6 @@ require_once __DIR__ . '/buttersafe.php';
 require_once __DIR__ . '/theawkwardyeti.php';
 //buni
 require_once __DIR__ . '/buni.php';
-
+//PennyArcade
+require_once __DIR__ . '/penny-arcade.php';
 ?>
